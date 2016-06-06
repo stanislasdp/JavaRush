10fe8fc695e48d277782ee6679cc6136c5039ccd
stas <forempty@gmail.com>
2016-06-06 23:53:38 +0300

    1

---
+++ test/level14/lesson08/home01/SuspensionBridge.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by stas on 6/6/16.
+ */
+public class SuspensionBridge implements Bridge
+{
+
+    @Override
+    public int getCarsCount()
+    {
+        return 100;
+    }
+}
---
+++ test/level14/lesson08/home01/WaterBridge.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by stas on 6/6/16.
+ */
+public class WaterBridge implements Bridge
+{
+    @Override
+    public int getCarsCount()
+    {
+        return 200;
+    }
+}
