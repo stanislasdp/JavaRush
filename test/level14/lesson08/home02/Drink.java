e4b767cdd38bfecf4cc0d22a9b8774b77d323d3c
stas <forempty@gmail.com>
2016-06-07 00:10:35 +0300

    1

---
+++ test/level14/lesson08/home02/Drink.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by stas on 6/6/16.
+ */
+public abstract class Drink
+{
+    public void taste()
+    {
+        System.out.println("Вкусно");
+    }
+}
---
+++ test/level14/lesson08/home02/Wine.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by stas on 6/6/16.
+ */
+public class Wine extends Drink
+{
+    public String getHolidayName()
+    {
+        return "День рождения";
+    }
+}
